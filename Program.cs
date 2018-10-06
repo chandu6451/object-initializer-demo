d0f76f1b5cb14777133ecbd7617d097c7a6855ea
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:48:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializerDemo
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee emp = new Employee();
+            //emp.EmpID = 101;
+            //emp.EmpName = "Robert";
+            //emp.Salary = 20000;
+
+            Employee emp = new Employee() { EmpID = 101, EmpName = "Robert", Salary = 20000 };
+            Console.WriteLine("Employee ID : " + emp.EmpID);
+            Console.WriteLine("Employee Name : " + emp.EmpName);
+            Console.WriteLine("Salary : " + emp.Salary);
+
+            Console.ReadKey();
+
+        }
+    }
+}
